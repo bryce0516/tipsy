1d8cff2f1fe1dacfead8db12417728a097ea6a62
bryce0516 <jayhspark@gmail.com>
2022-05-23 22:31:53 +0900

    update

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+
+    @IBAction func tipChanged(_ sender: Any) {
     }
-
-
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+    }
 }
 
